fa91b0d078271106d2dd97c1627e0917b4a0e3d6
Geno McNew <genemcnew3@gmail.com>
2021-03-11 13:06:19 -0600

    added getting all media by a window of dates authed, latest episode two weeks for unauthed

--- mmsapi/serializers/medias.py
+++ mmsapi/serializers/medias.py
@@ -4,4 +4,4 @@ from mmsapi.models import Media
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
-        fields = ('title', 'description', 'poster_url', 'release_date', 'media_type', 'external_id')
+        fields = ('id', 'title', 'description', 'poster_url', 'release_date', 'media_type', 'external_id')
